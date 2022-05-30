20b9dda7b24ef046e7d7cca5829eb919b3db0547
Erik Sauerbier <erik.sauerbier@mailbox.org>
2022-05-30 12:57:40 +0200

    v.1.1
    - Ausgabetext angepasst
    - Skript-Kommentare optimiert

---
+++ PRTG-Show-blocked-RDSessionhosts.ps1
@@ -0,0 +1,29 @@
+﻿# PRTG-Skript zum Anzeigen der RD-Sessionhosts mit gesperrter Anmeldung
+# Stannek GmbH - v.1.1 - 30.05.2022 - E.Sauerbier
+
+# Beachte dass dieses Skript nur mit der 64Bit Powershell läuft, 
+# hierzu muss ein mittels PRTG ein "Start-Skript" gestartet werden, da die Probe nur als 32Bit-Version verfügbar ist (Stand 05.2022)
+
+# RD-SessionHosts auslesen
+$SessionHost = Get-RDSessionCollection | Get-RDSessionHost
+
+# gesperrte Hosts auslesen
+$blockedHosts = $SessionHost | Where NewConnectionAllowed -eq No
+
+# Ausgabetext generieren 
+if ($blockedHosts -eq $Null) {$OutputText = "Es sind keine Sessionhosts gesperrt"}
+Else {$OutputText = "An folgenden Sessionhosts ist die Anmeldung gesperrt: " + $blockedHosts.SessionHost}
+
+# XML Ausgabe für PRTG
+"<?xml version=""1.0"" encoding=""UTF-8"" ?>"
+"<prtg>"
+"<result>" 
+"<channel>Gesperrte Sessionhosts</channel>" 
+"<value>"+$blockedHosts.Count+"</value>"
+"</result>"
+"<result>"
+"<channel>Anzahl der Sessionhosts in der Sammlung</channel>"    
+"<value>"+$SessionHost.Count+"</value>"
+"</result>"
+"<text>" + $OutputText +"</text>"
+"</prtg>"
