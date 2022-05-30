19c1fad8ef675ba4cc0bd4f671565a8f6c9c3075
Erik Sauerbier <erik.sauerbier@mailbox.org>
2022-05-30 12:57:57 +0200

    Create PRTG-Start-x64CustomScripts.ps1
    - First Release

---
+++ PRTG-Start-x64CustomScripts.ps1
@@ -0,0 +1,14 @@
+﻿# PRTG-Skript zum starten von 64Bit Powershell Skripts
+# Stannek GmbH - v.1.0 - 30.05.2022 - E.Sauerbier
+
+# Paramter die vom Sensor mit gegeben werden
+param([string]$ScriptPath = $Null,[string]$ScriptName = " ")
+
+# Parameter
+$PRTGPRobeScriptPath = "C:\Program Files (x86)\PRTG Network Monitor\Custom Sensors\EXEXML\"
+
+# Wenn kein Skriptpfad mitgegeben wurde, dann wird der Standard PRTG-Skriptpfad genutzt
+If (($ScriptPath -eq $Null) -or ($ScriptPath -eq "")) {$ScriptPath = $PRTGPRobeScriptPath}
+
+# 64Bit Powershell mit dem entsprechenden Skript starten
+c:\windows\Sysnative\windowspowershell\v1.0\powershell.exe -file ($ScriptPath + $ScriptName)
