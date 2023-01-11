64189d2dabcff7525e5f4571fa64c3114b234999
eriksauerbier <erik.sauerbier@mailbox.org>
2023-01-11 17:17:45 +0100

    v.1.1
    - Text Ausgabe optimiert
    - Powershell-Abfrage erfolgt nun mit Credentials, die per PRTG mitgegeben werden
    - Domäne für Clusterabfrage wird nun per PRTG  mitgegeben

--- PRTG-CheckConnectedLUNs.ps1
+++ PRTG-CheckConnectedLUNs.ps1
@@ -1,14 +1,17 @@
 ﻿# Dieses PRTG-Skript prüft wieviele LUNs pro Hyper-V Host an jedem ClusterNode verbunden sind und schlägt bei ungleichheit an
-# Stannek GmbH - E.Sauerbier - v.1.02 - 16.12.2021
+# Stannek GmbH - E.Sauerbier - v.1.1 - 11.01.2023
 
 # Parameter für den PRTG-Sensor (ClusterNode auf dem das Skript ausgeführt wird und ClusterName
-param([string]$VMHost = "N/A",[string]$Cluster = "N/A")
+param([string]$VMHost = "N/A",[string]$Cluster = "N/A",[string]$Password = ' ',$Admin = " ",[string]$Domain = ' ')
 
 # Fehlervariable zurücksetzen
 $failureHost = $null
 
+# Credentials für PSRemote-Befehl erstellen
+$PSCred = New-Object System.Management.Automation.PSCredential -ArgumentList $Admin,(ConvertTo-SecureString -AsPlainText $Password -Force)
+
 # ClusterNodes abfragen
-$ClusterNodes = Invoke-Command -cn $VMHost -ScriptBlock {
+$ClusterNodes = Invoke-Command -ComputerName $VMHost -Credential $PSCred -ScriptBlock {
 $ClusterNodes = Get-ClusterNode | Select-Object -ExpandProperty Name
 return $ClusterNodes 
 }
@@ -22,7 +25,7 @@ $LUNCompareValue = $ClusterDisks.Count + "1"
 foreach ($ClusterNode in $ClusterNodes){
 # HostName erfassen
 # Anzahl der verbundenen LUNs auf dem Host in der Schleife erfassen
-$ClusterNode = $ClusterNode + ".rzdom.local"
+$ClusterNode = $ClusterNode + "." + $Domain
 $LunCount = invoke-command -computername $ClusterNode -scriptblock {
 $Luns = (Get-WmiObjec -Namespace root\wmi -Class mpio_disk_info).driveinfo | Select-Object Name
 $Luns.Count
@@ -31,11 +34,15 @@ $Luns.Count
 If ($LUNCompareValue -ne $LunCount) {$failureHost += @($ClusterNode)}
 }
 
+# Ausgabe-Text generieren
+If ($failureHost.count -eq "0") {$OutPutText = "Die MPIO-LUN Verbindungen sind OK"}
+Else {$OutPutText = "An " + $failureHost.count + " Hosts passen die MPIO-LUN Verbindungen nicht ("+$failureHost+")"}
+
 # Ausgabe für PRTG
 "<prtg>"
 "<result>" 
 "<channel>Host mit fehlerhafter LUN-Verbindung</channel>"    
 "<value>"+ $failureHost.Count +"</value>" 
 "</result>"
-"<text> An " + $failureHost.count + " Hosts passen die MPIO-LUN Verbindungen nicht ("+$failureHost+")</text>"
+"<text>"+$OutPutText+"</text>"
 "</prtg>"
