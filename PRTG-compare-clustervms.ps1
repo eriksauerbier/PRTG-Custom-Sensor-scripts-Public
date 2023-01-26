da246e5f7a736d9d15a7e209bdebb4f82ee1ff7f
eriksauerbier <erik.sauerbier@mailbox.org>
2023-01-26 13:15:54 +0100

    v.1.1.2
    - Abruf erfolgt nun mit Invoke-Command
    - PRTG-Parameter für Benutzername und Passwort hinzugefügt
    - Ausgabe Text optimiert

--- PRTG-compare-clustervms.ps1
+++ PRTG-compare-clustervms.ps1
@@ -1,14 +1,13 @@
 ﻿# Skript zum Vergleich der Cluster-VMs und der in VeeamBackupJobs befindlichen Cluster-VMs
-# Stannek GmbH - v.1.1 - 12.05.2022 - E.Sauerbier
+# Stannek GmbH - v.1.1.2 - 26.01.2023 - E.Sauerbier
 
 # Parameter für den PRTG-Sensor
-param([string]$remoteserver = "N/A")
+param([String]$remoteserver = "",[string]$User = "",[string]$Password = '')
 
-# PS-Session am Backup-Server öffnen
-invoke-command -computername $remoteserver -scriptblock {
-
-# Veeam PS-Snapin laden
-Add-PSSnapin VeeamPSSnapin
+# Skriptblock erstellen
+$ScriptBlock = {
+# Powershell Modul importieren
+Import-Module Veeam.Backup.PowerShell
 
 # Aktive Veeam VM Backup-Jobs auslesen
 $Jobnames = Get-VBRJob | Where-Object {($_.JobType -eq "Backup") -and ($_.IsScheduleEnabled -eq "True")} | Select-Object Name
@@ -16,31 +15,50 @@ $Jobnames = Get-VBRJob | Where-Object {($_.JobType -eq "Backup") -and ($_.IsSche
 # VM-Namen der Backup-Jobs in Variable schreiben
 $Jobobjects = foreach ($Jobname in $Jobnames) {Get-VBRJobObject -Job $Jobname.Name | Select Name}
 
+# Cluster auslesen
+$Cluster = Get-Cluster -Domain $env:UserDomain
+
 # Cluster-VMs auslesen
-$ClusterVM = Get-ClusterResource -Cluster RZCLuster | Where ResourceType -eq "Virtual Machine" | Select OwnerGroup
+$ClusterVM = Get-ClusterResource -Cluster $Cluster | Where ResourceType -eq "Virtual Machine" | Select OwnerGroup
 
 # Namen der Cluster-VMs in Variable schreiben
 $NameClusterVM = $ClusterVM | ForEach-Object {$_.OwnerGroup}
 
+$object = New-Object -TypeName PSObject
+$object | Add-Member –MemberType NoteProperty –Name ClusterVM –Value $NameClusterVM
+$object | Add-Member –MemberType NoteProperty –Name CountClusterVM –Value $NameClusterVM.Count
+$object | Add-Member –MemberType NoteProperty –Name JobVMs –Value $Jobobjects
+$object | Add-Member –MemberType NoteProperty –Name CountJobVM –Value $Jobobjects.Count
+
+return $object
+}
+
+# Skriptblock ausführen, je nach mitgegebene Parametern
+If ($remoteserver -eq "") {$Output = Invoke-Command -ScriptBlock $ScriptBlock}
+Else {$Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $User,$(ConvertTo-SecureString -AsPlainText $Password -Force)
+      $Output = Invoke-Command -ComputerName $remoteserver -Credential $Cred -ScriptBlock $ScriptBlock}
+
 # Vergleichen der Cluster-VMs und der in VeeamBackupJobs befindlichen Cluster-VMs
-$output = Compare-Object -ReferenceObject $NameClusterVM.Name -DifferenceObject $Jobobjects.Name | Foreach-object InputObject
+$Compare = Compare-Object -ReferenceObject $Output.ClusterVM.Name -DifferenceObject $Output.JobVMs.Name | Foreach-object InputObject
+
+# PRTG Ausgabetext generieren
+If ($Compare.Count -eq "0") {$OutputText = "Das Hyper-V Cluster hat "+$Output.CountClusterVM+" VMs und davon werden "+ $Output.CountJobVM +" Cluster-VMs gesichert"}
+Else {$OutputText = "Folgende VMs werden nicht gesichert: $($Compare)"}
 
 # XML Ausgabe für PRTG
-#"<?xml version=`"1.0`" encoding=`"UTF-8`" ?>"
+"<?xml version=`"1.0`" encoding=`"UTF-8`" ?>"
 "<prtg>"
 "<result>" 
 "<channel>nicht gesicherte ClusterVMs</channel>"    
-"<value>"+ $output.Count +"</value>" 
+"<value>"+ $Compare.Count +"</value>" 
 "</result>"
 "<result>" 
 "<channel>Anzahl Cluster VMs</channel>"    
-"<value>"+ $ClusterVM.Count +"</value>" 
+"<value>"+ $Output.CountClusterVM +"</value>" 
 "</result>"
 "<result>" 
 "<channel>Anzahl VMs in Backupjobs</channel>"    
-"<value>"+ $Jobobjects.Count +"</value>" 
+"<value>"+ $Output.CountJobVM +"</value>" 
 "</result>"
-"<text>Das Hyper-V Cluster hat"+$ClusterVM.Count+" VMs und davon werden "+ $Jobobjects.Count +" Cluster.VMs gesichert</text>"
+"<text>$OutputText</text>"
 "</prtg>"
-
-}
