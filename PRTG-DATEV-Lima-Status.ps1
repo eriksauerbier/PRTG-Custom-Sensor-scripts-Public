abdb1ec2a42c638d87db98dba5a41cfd64ab5f1b
Erik Sauerbier <erik.sauerbier@mailbox.org>
2023-10-20 14:27:39 +0200

    v.2.5
    - Ausgabe für PRTG PowerShell Security Enhancement optimiert
    - Variablen Namen angepasst
    - Sensortext optimiert
    - Skriptbeschreibung optimiert

--- PRTG-DATEV-Lima-Status.ps1
+++ PRTG-DATEV-Lima-Status.ps1
@@ -1,42 +1,43 @@
-# PRTG-Skript zum prüfen des DATEV Lima-Status
-# Stannek GmbH - E.Sauerbier - v.2.05 - 23.11.2021
+# PRTG-Skript zum pruefen des DATEV Lima-Status
+# Stannek GmbH - E.Sauerbier - v.2.5 - 20.10.2023
 
-# Servername für den PRTG-Sensor, falls der Lima nicht auf der PRTG-Probe läuft
-param([string]$LiMaServer = "N/A")
+# Servername fuer den PRTG-Sensor, falls der Lima nicht auf der PRTG-Probe laeuft
+param([string]$LiMaServer = "$env:COMPUTERNAME")
 
 # Pfad zum DATEV LimaStatus-Tool
+$PathLimaStatusTool = Join-Path -Path $Env:DATEVPP -ChildPath "\PROGRAMM\Sws\LiMaStatus.exe"
 
-$PathLimaStatusTool = "\PROGRAMM\Sws\LiMaStatus.exe"
-$LimaStatusTool = $Env:DATEVPP + $PathLimaStatusTool
-
-# DATEV Tool LiMaStatus starten
-
-If (Test-Path $LimaStatusTool) 
+# DATEV Tool LiMaStatus entsprechend starten
+If (Test-Path $PathLimaStatusTool) 
 {
-$process = Start-Process -FilePath $LimaStatusTool -NoNewWindow -Wait -PassThru
+$Process = Start-Process -FilePath $PathLimaStatusTool -NoNewWindow -Wait -PassThru
 }
 Else {
-# Falls der Lima nicht an der PRTG-Probe läuft, wird eine Remote-Aufruf durchgeführt
-$process = Invoke-Command -cn $LiMaServer -ScriptBlock {param($PathLimaStatusTool=$PathLimaStatusTool)
-            $LimaStatusTool = $Env:DATEVPP + $PathLimaStatusTool
-            $result = Start-Process -FilePath $LimaStatusTool -NoNewWindow -Wait -PassThru
+# Falls der Lima nicht an der PRTG-Probe laeuft, wird eine Remote-Aufruf durchgefuehrt
+$Process = Invoke-Command -cn $LiMaServer -ScriptBlock {param($PathLimaStatusTool=$PathLimaStatusTool)
+            $result = Start-Process -FilePath $PathLimaStatusTool -NoNewWindow -Wait -PassThru
             New-Object -TypeName PSCustomObject -Property @{Exitcode=$result.ExitCode}
            } -ArgumentList $PathLimaStatusTool
 }
 
 # Ergebnis des DATEV Tools auswerten
-if ($process.ExitCode -eq $Null ) {$prtgtext= "Fehler beim Aufruf vom Tool (LiMaStatus.exe) auf folgendem Server: $LiMaServer";$LASTEXITCODE = "5"}
-if ($process.ExitCode -eq "4" ) {$prtgtext= "POOL_INKONSISTENT";$LASTEXITCODE = "4"}
-if ($process.ExitCode -eq "3" ) {$prtgtext= "Falsche DLL";$LASTEXITCODE = "3"}
-if ($process.ExitCode -eq "2" ) {$prtgtext= "Falscher Parameter";$LASTEXITCODE = "2"}
-if ($process.ExitCode -eq "1" ) {$prtgtext= "Lima Gestoppt";$LASTEXITCODE = "1"}
-if ($process.ExitCode -eq "0" ) {$prtgtext= "Lima-Status OK";$LASTEXITCODE = "0"}
+if ($Process.ExitCode -eq $Null ) {$TextPRTGSensor= "Fehler beim Aufruf vom Tool (LiMaStatus.exe) auf folgendem Server: $LiMaServer";$ExitCodeLima = "5"}
+if ($Process.ExitCode -eq "4" ) {$TextPRTGSensor= "POOL_INKONSISTENT";$ExitCodeLima = "4"}
+if ($Process.ExitCode -eq "3" ) {$TextPRTGSensor= "Falsche DLL";$ExitCodeLima = "3"}
+if ($Process.ExitCode -eq "2" ) {$TextPRTGSensor= "Falscher Parameter";$ExitCodeLima = "2"}
+if ($Process.ExitCode -eq "1" ) {$TextPRTGSensor= "Lima Gestoppt";$ExitCodeLima = "1"}
+if ($Process.ExitCode -eq "0" ) {$TextPRTGSensor= "Lima-Status OK";$ExitCodeLima = "0"}
 
-# XML Ausgabe für PRTG
-"<prtg>"
-"<result>"
-"<channel>Lima-Status</channel>"
-"<value>"+$LASTEXITCODE+"</value>"
-"</result>"
-"<text>$prtgtext</text>"
-"</prtg>"
+
+# Ausgabe-Variable fuer PRTG erzeugen
+$OutputStringXML = "<?xml version=`"1.0`"?>`n"
+$OutputStringXML += "<prtg>`n"
+$OutputStringXML += "<result>`n" 
+$OutputStringXML += "<channel>Lima-Status</channel>`n" 
+$OutputStringXML += "<value>"+ $ExitCodeLima +"</value>`n" 
+$OutputStringXML += "</result>`n"
+$OutputStringXML += "<text>" + $TextPRTGSensor  + "</text>`n"
+$OutputStringXML += "</prtg>"
+
+# Ausgabe fuer PRTG
+Write-Output -InputObject $OutputStringXML
