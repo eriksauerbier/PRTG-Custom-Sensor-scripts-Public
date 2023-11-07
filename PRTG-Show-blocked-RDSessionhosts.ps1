38ab374e91cde0c7120c0945b905b60874cc403d
eriksauerbier <94852703+eriksauerbier@users.noreply.github.com>
2023-11-07 15:48:50 +0100

    v.1.2
    - Ausgabe für PRTG PowerShell Security Enhancement optimiert

--- PRTG-Show-blocked-RDSessionhosts.ps1
+++ PRTG-Show-blocked-RDSessionhosts.ps1
@@ -1,5 +1,5 @@
 ﻿# PRTG-Skript zum Anzeigen der RD-Sessionhosts mit gesperrter Anmeldung
-# Stannek GmbH - v.1.1 - 30.05.2022 - E.Sauerbier
+# Stannek GmbH - v.1.2 - 07.11.2022 - E.Sauerbier
 
 # Beachte dass dieses Skript nur mit der 64Bit Powershell läuft, 
 # hierzu muss ein mittels PRTG ein "Start-Skript" gestartet werden, da die Probe nur als 32Bit-Version verfügbar ist (Stand 05.2022)
@@ -8,22 +8,25 @@
 $SessionHost = Get-RDSessionCollection | Get-RDSessionHost
 
 # gesperrte Hosts auslesen
-$blockedHosts = $SessionHost | Where NewConnectionAllowed -eq No
+$BlockedHosts = $SessionHost | Where NewConnectionAllowed -eq No
 
 # Ausgabetext generieren 
-if ($blockedHosts -eq $Null) {$OutputText = "Es sind keine Sessionhosts gesperrt"}
-Else {$OutputText = "An folgenden Sessionhosts ist die Anmeldung gesperrt: " + $blockedHosts.SessionHost}
+if ($Null -eq $blockedHosts) {$TextPRTGSensor = "Es sind keine Sessionhosts gesperrt"}
+Else {$TextPRTGSensor = "An folgenden Sessionhosts ist die Anmeldung gesperrt: " + $blockedHosts.SessionHost}
 
-# XML Ausgabe für PRTG
-"<?xml version=""1.0"" encoding=""UTF-8"" ?>"
-"<prtg>"
-"<result>" 
-"<channel>Gesperrte Sessionhosts</channel>" 
-"<value>"+$blockedHosts.Count+"</value>"
-"</result>"
-"<result>"
-"<channel>Anzahl der Sessionhosts in der Sammlung</channel>"    
-"<value>"+$SessionHost.Count+"</value>"
-"</result>"
-"<text>" + $OutputText +"</text>"
-"</prtg>"
+# Ausgabe-Variable fuer PRTG erzeugen
+$OutputStringXML = "<?xml version=`"1.0`"?>`n"
+$OutputStringXML += "<prtg>`n"
+$OutputStringXML += "<result>`n" 
+$OutputStringXML += "<channel>Gesperrte Sessionhosts</channel>`n" 
+$OutputStringXML += "<value>"+$BlockedHosts.Count+"</value>`n" 
+$OutputStringXML += "</result>`n"
+$OutputStringXML += "<result>`n" 
+$OutputStringXML += "<channel>Anzahl der Sessionhosts in der Sammlung</channel>`n" 
+$OutputStringXML += "<value>"+$SessionHost.Count+"</value>`n" 
+$OutputStringXML += "</result>`n"
+$OutputStringXML += "<text>"+$TextPRTGSensor+"</text>`n"
+$OutputStringXML += "</prtg>"
+
+# Ausgabe fuer PRTG
+Write-Output -InputObject $OutputStringXML
