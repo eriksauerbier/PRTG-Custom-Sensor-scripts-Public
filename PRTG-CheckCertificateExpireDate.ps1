4cf8a54027bb33fb25425afbfc50997224dd9ca0
eriksauerbier <94852703+eriksauerbier@users.noreply.github.com>
2023-11-07 16:32:37 +0100

    v.1.5
    - Ausgabe für PRTG PowerShell Security Enhancement optimiert
    - Skriptkommentare optimiert

--- PRTG-CheckCertificateExpireDate.ps1
+++ PRTG-CheckCertificateExpireDate.ps1
@@ -1,9 +1,8 @@
 ﻿# Dieses PRTG-Skript sucht nach Zertifikaten die in X-Tagen ablaufen
-# Stannek GmbH - E.Sauerbier - v.1.41 - 16.12.2021
+# Stannek GmbH - E.Sauerbier - v.1.5 - 07.11.2023
 
 # Servername für PRTG-Sensor, falls der Cert-Server nicht auf der PRTG-Probe läuft
 param([string]$Server=$Null,[string]$CertPath="Cert:\LocalMachine\My",[string]$sincedays="31")
-
 
 # Beschreibung
 # $Server = Hier den Servernamen angeben
@@ -14,25 +13,27 @@ param([string]$Server=$Null,[string]$CertPath="Cert:\LocalMachine\My",[string]$s
 $searchdate =  Get-Date -format yyyy-MM-dd ((Get-Date).addDays($sincedays))  
 
 # Remote-Abfrage der Ablaufdaten der Zertifikate im Zertifikatsspeicher
-
 $expiredCert = Invoke-Command -cn $Server -ScriptBlock { param($CertPath,$searchdate)
 get-childitem -Recurse $CertPath | ?{$_.NotAfter -lt $searchdate} | Select-Object -ExpandProperty Subject
 } -ArgumentList $CertPath,$searchdate -ErrorAction Stop
 
-
+# Wert frü PRTG-Sensor erzeugen
 $prtgvalue = "0"
 if ($expiredCert.Count -ge 1) {$prtgvalue = $expiredCert.Count}
 if ($expiredCert.Count -eq 0) {$expiredCert = "Keine"}
 
-$prtgtext = "Folgende Zertifikat laufen in weniger als $sincedays Tagen ab: $expiredCert"
+# Text für Sensor erzeugen
+$TextPRTGSensor = "Folgende Zertifikat laufen in weniger als $sincedays Tagen ab: $expiredCert"
 
+# Ausgabe-Variable fuer PRTG erzeugen
+$OutputStringXML = "<?xml version=`"1.0`"?>`n"
+$OutputStringXML += "<prtg>`n"
+$OutputStringXML += "<result>`n" 
+$OutputStringXML += "<channel>Expired Certificate</channel>`n" 
+$OutputStringXML += "<value>"+$prtgvalue+"</value>`n" 
+$OutputStringXML += "</result>`n"
+$OutputStringXML += "<text>"+$TextPRTGSensor+"</text>`n"
+$OutputStringXML += "</prtg>"
 
-# Ausgabe für PRTG
-"<?xml version=`"1.0`" encoding=`"UTF-8`" ?>"
-"<prtg>"
-"<result>"
-"<channel>Expired Certificate</channel>"
-"<value>$prtgvalue</value>"
-"</result>"
-"<text>$prtgtext</text>"
-"</prtg>"
+# Ausgabe fuer PRTG
+Write-Output -InputObject $OutputStringXML
