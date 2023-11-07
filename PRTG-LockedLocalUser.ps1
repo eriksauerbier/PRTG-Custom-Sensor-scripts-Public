bfb336f18aad7ae8135aa0c5446f626badf201c3
eriksauerbier <94852703+eriksauerbier@users.noreply.github.com>
2023-11-07 16:29:21 +0100

    v.1.1
    - Ausgabe für PRTG PowerShell Security Enhancement optimiert
    - Sensortext optimiert

--- PRTG-LockedLocalUser.ps1
+++ PRTG-LockedLocalUser.ps1
@@ -1,16 +1,23 @@
 # Dieses PRTG-Skript prüft ob ein Lokaler Benutzer gesperrt ist
-# Stannek GmbH - E.Sauerbier - v.1.01 - 23.11.2021
+# Stannek GmbH - E.Sauerbier - v.1.1 - 07.11.2023
 
 # Lokale User Abfrage
 $lockedAccounts = @(Get-WmiObject win32_useraccount -filter "LockOut=True")
 $lockedAccounts.name
 
+# Text für Ausgabe generieren
+if ($lockedAccounts.count -gt "0") {$TextPRTGSensor = "Folgende lokale Benutzer sind gesperrt: "+$lockedAccounts.name}
+Else {$TextPRTGSensor = "Es sind keine lokalen Benutzer gesperrt"}
 
-# Ausgabe für PRTG
-"<prtg>"
-"<result>" 
-"<channel>Locked Users</channel>"    
-"<value>"+ $lockedAccounts.count +"</value>" 
-"</result>"
-"<text>" + $lockedAccounts.name + "</text>"
-"</prtg>"
+# Ausgabe-Variable fuer PRTG erzeugen
+$OutputStringXML = "<?xml version=`"1.0`"?>`n"
+$OutputStringXML += "<prtg>`n"
+$OutputStringXML += "<result>`n" 
+$OutputStringXML += "<channel>Locked Users</channel>`n" 
+$OutputStringXML += "<value>"+$lockedAccounts.count+"</value>`n" 
+$OutputStringXML += "</result>`n"
+$OutputStringXML += "<text>"+$TextPRTGSensor+"</text>`n"
+$OutputStringXML += "</prtg>"
+
+# Ausgabe fuer PRTG
+Write-Output -InputObject $OutputStringXML
