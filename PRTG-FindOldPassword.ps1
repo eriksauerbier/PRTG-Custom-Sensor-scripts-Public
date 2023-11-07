7ef66f6e7ecbf963582fbc5cb40ac6f04493c3fa
eriksauerbier <94852703+eriksauerbier@users.noreply.github.com>
2023-11-07 16:27:29 +0100

    v.1.2
    - Ausgabe für PRTG PowerShell Security Enhancement optimiert

--- PRTG-FindOldPassword.ps1
+++ PRTG-FindOldPassword.ps1
@@ -1,5 +1,5 @@
 ﻿# Dieses PRTG-Skript sucht nach nicht geänderten Userpasswörtern entsprechend der Passwortrichtlinien des Kundens.
-# Stannek GmbH - E.Sauerbier - v.1.1 - 15.05.2022
+# Stannek GmbH - E.Sauerbier - v.1.2 - 07.11.2023
 
 # Parameter für den PRTG-Sensor
 param([string]$sincedays = "0",[string]$searchbase1 = " ",[string]$searchbase2 = " ")
@@ -24,19 +24,22 @@ $Query2 = Get-ADUser -filter { Enabled -eq $True } –Properties PwdLastSet -sea
 # Werte für die Ausgabe generieren
 $Oldpasswords = $Query1.Count + $Query2.Count
 $Users = $Query1.sAmAccountName + $Query2.sAmAccountName
-If ($Oldpasswords -ne "0") {$Outputtext = "Folgende User haben Ihr Kennwort seit mehr als $sincedays Tagen nicht geaendert: $Users"}
-Else {$Outputtext = "Alle Benutzer habe Ihr Kennwort vor $sincedays Tagen geaendert"}
+If ($Oldpasswords -ne "0") {$TextPRTGSensor = "Folgende User haben Ihr Kennwort seit mehr als $sincedays Tagen nicht geaendert: $Users"}
+Else {$TextPRTGSensor = "Alle Benutzer habe Ihr Kennwort vor $sincedays Tagen geaendert"}
 
 # Fehlerabfrage
 If ($failure -ne $Null) {$Oldpasswords = "1"; $Users = "Es ist ein Fehler aufgetreten"}
 Else {If (($Query1 -eq " ") -and ($Query2 -eq " ")) {$Oldpasswords = "1"; $Users = "Es ist ein Fehler aufgetreten"}}
 
-# XML Ausgabe für PRTG
-"<?xml version=`"1.0`" encoding=`"UTF-8`" ?>"
-"<prtg>"
-"<result>"
-"<channel>Passwort zu alt</channel>"
-"<value>$Oldpasswords</value>"
-"</result>"
-"<text>$Outputtext</text>"
-"</prtg>"
+# Ausgabe-Variable fuer PRTG erzeugen
+$OutputStringXML = "<?xml version=`"1.0`"?>`n"
+$OutputStringXML += "<prtg>`n"
+$OutputStringXML += "<result>`n" 
+$OutputStringXML += "<channel>Passwort zu alt</channel>`n" 
+$OutputStringXML += "<value>"+$Oldpasswords+"</value>`n" 
+$OutputStringXML += "</result>`n"
+$OutputStringXML += "<text>"+$TextPRTGSensor+"</text>`n"
+$OutputStringXML += "</prtg>"
+
+# Ausgabe fuer PRTG
+Write-Output -InputObject $OutputStringXML
