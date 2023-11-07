f4ab056c39b06e86d6606a97010a5891c76eec6f
eriksauerbier <94852703+eriksauerbier@users.noreply.github.com>
2023-11-07 15:35:03 +0100

    v.1.2
    - Ausgabe für PRTG PowerShell Security Enhancement optimiert

--- PRTG-ShowSnapshotsOnHyperVHost.ps1
+++ PRTG-ShowSnapshotsOnHyperVHost.ps1
@@ -1,5 +1,5 @@
 ﻿# PRTG-Skript zum Abfragen von vorhandenen Snaptshot auf einem Hyper-V Host
-# Stannek GmbH - v.1.1 - 11.01.2023 - E.Sauerbier
+# Stannek GmbH - v.1.2 - 07.11.2023 - E.Sauerbier
 
 
 # Parameter für den PRTG-Sensor
@@ -7,24 +7,28 @@ param([string]$HyperVHost = " ",[string]$Password = ' ',$Admin = " ")
 
 $PSSCred = New-Object System.Management.Automation.PSCredential -ArgumentList $Admin,$(ConvertTo-SecureString -AsPlainText $Password -Force)
 
-# Alle VirtuelleMaschinen am Host nach Snapshots abfragen
+# Alle Virtuelle Maschinen am Host nach Snapshots abfragen
 $SnapShots = Invoke-Command -ComputerName $HyperVHost -Credential $PSSCred -ErrorVariable ConnectError -ScriptBlock {Get-VM | Get-VMSnapshot | Select-Object VMName,Name,SnapshotType,CreationTime,ComputerName}
 
 # Anzahl der Snapshots in PRTG Value schreiben
 $PRTGValue = $SnapShots.VMName.Count
 
 # Ausgabe-Text generieren
-If ($Null -eq $SnapShots) {$OutPutText = "Es befinden sich keine Snapshots auf dem Host"}
-Else {$OutPutText = "Folgende VM(s) hat/haben Snapshot(s): $($SnapShots.VMName)"}
+If ($Null -eq $SnapShots) {$TextPRTGSensor = "Es befinden sich keine Snapshots auf dem Host"}
+Else {$TextPRTGSensor = "Folgende VM(s) hat/haben Snapshot(s): $($SnapShots.VMName)"}
 
 # Fehlerbehandlung
-If ($ConnectError -ne $Null) {$OutPutText = "Es ist ein Fehler bei der VM-Abfrage aufgetreten";$PRTGValue="1"}
+If ($ConnectError -ne $Null) {$TextPRTGSensor = "Es ist ein Fehler bei der VM-Abfrage aufgetreten";$PRTGValue="1"}
 
-# Ausgabe für PRTG
-"<prtg>"
-"<result>" 
-"<channel>Snapshots</channel>"    
-"<value>"+$PRTGValue+"</value>" 
-"</result>"
-"<text>"+$OutPutText+"</text>"
-"</prtg>"
+# Ausgabe-Variable fuer PRTG erzeugen
+$OutputStringXML = "<?xml version=`"1.0`"?>`n"
+$OutputStringXML += "<prtg>`n"
+$OutputStringXML += "<result>`n" 
+$OutputStringXML += "<channel>Snapshots</channel>`n" 
+$OutputStringXML += "<value>"+$PRTGValue+"</value>`n" 
+$OutputStringXML += "</result>`n"
+$OutputStringXML += "<text>"+$TextPRTGSensor+"</text>`n"
+$OutputStringXML += "</prtg>"
+
+# Ausgabe fuer PRTG
+Write-Output -InputObject $OutputStringXML
